643e58993785da926ed70d2b1e4286ee306b48cd
salen <salenju@163.com>
2019-07-02 23:38:19 +0800

    添加配置文件 + 基本数据类型

---
+++ code/app.js
@@ -0,0 +1,18 @@
+"use strict";
+// 基本数据类型
+var num = 25;
+var flo = 25.5;
+var hex = 0xf000;
+// 重新赋值
+// num = '25';  // 不能存储非原有类型的数据
+num = 24.8;
+// ts原型
+var num2 = 28; // 等同于  let num2：number = 28;
+// boolean
+var isLogin = false;
+// isLogin = 1;   // 报错
+// string
+var str = 'Hello Salen';
+// any
+var anything = 28;
+anything = 'Hello';
